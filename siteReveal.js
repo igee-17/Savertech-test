088142716ddc830a0dc5ab1ba997a298def3dddb
Igee <igbagboleye@gmail.com>
2022-07-23 23:32:15 +0100

    cleanup of remaining norders

--- siteReveal.js
+++
@@ -1,9 +0,0 @@
-const revealSite = () => {
-  window.addEventListener("scroll", reveal);
-  function reveal() {
-    const reveals = [...document.querySelectorAll(".reveal")];
-    console.log(reveals);
-  }
-};
-
-export default revealSite;
